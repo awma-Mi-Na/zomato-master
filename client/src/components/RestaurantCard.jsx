c77f215d0e126a7a8c6fcb0445cd2cc4f0ca24fc
awma-Mi-Na <awmach.9@gmail.com>
2021-10-13 14:49:01 +0530

    food tab with props

---
+++ client/src/components/RestaurantCard.jsx
@@ -0,0 +1,48 @@
+import { AiTwotoneStar } from "react-icons/ai";
+
+const RestaurantCard = (props) => {
+  return (
+    <>
+      <div className="bg-white p-4 mb-4 w-full rounded-2xl md:w-1/2 lg:w-1/3 transition duration-700 ease-in-out hover:shadow-lg">
+        <div className="w-full h-56 lg:h-64 relative">
+          <div className="absolute w-full flex items-end justify-between bottom-4">
+            <div className="flex flex-col items-start gap-2">
+              {props.isPro && (
+                <span className="bg-zmtcolor-400 p-1 rounded text-white text-sm">
+                  Pro extra {`${props.isPro}`}% off
+                </span>
+              )}
+              {props.isOff && (
+                <span className="bg-blue-500 p-1 rounded text-white text-sm">
+                  {`${props.isOff}`}% off
+                </span>
+              )}
+            </div>
+            <span className="bg-white bg-opacity-75 p-1 rounded mr-2">
+              {`${props.durationOfDelivery}`} min
+            </span>
+          </div>
+          <img
+            src={props.photos.length && props.photos[0]}
+            alt="food"
+            className="w-full h-full object-cover rounded-2xl"
+          />
+        </div>
+        <div className="mt-2 flex flex-col gap-1">
+          <div className="flex items-center justify-between">
+            <h4 className="text-xl font-medium">{props.name}</h4>
+            <span className="bg-green-800 text-white text-sm p-1 rounded flex items-center ">
+              {props.restaurantReviewValue} <AiTwotoneStar />
+            </span>
+          </div>
+          <div className="flex items-center justify-between text-gray-500 text-sm">
+            <p>{props.cuisine.join(", ")}</p>
+            <p>₹{props.averageCost} for one</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default RestaurantCard;
--- client/src/components/delivery/index.jsx
+++ client/src/components/delivery/index.jsx
@@ -1,9 +1,59 @@
 import DeliveryCarousel from "./DeliveryCarousel";
+import Brand from "./Brand";
+import RestaurantCard from "../RestaurantCard";
+import { useState } from "react";
 
 const Delivery = () => {
+  const [restList] = useState([
+    {
+      _id: "12345",
+      name: "Kerela Food Club",
+      isPro: 15,
+      isOff: 30,
+      durationOfDelivery: 57,
+      photos: [
+        "https://b.zmtcdn.com/data/pictures/chains/1/18975421/ea98e99cc8c5e6d0bb5688d10e3b4f9d_o2_featured_v2.jpg?output-format=webp",
+      ],
+      restaurantReviewValue: 4.7,
+      cuisine: ["Kerela", "Seafood"],
+      averageCost: 200,
+    },
+    {
+      _id: "12346",
+      name: "FreshMenu",
+      isPro: 20,
+      isOff: 50,
+      durationOfDelivery: 51,
+      photos: [
+        "https://b.zmtcdn.com/data/pictures/chains/9/59179/a27b0a7128ad0035df8c14fabf52cdef_o2_featured_v2.jpg?output-format=webp",
+      ],
+      restaurantReviewValue: 3.8,
+      cuisine: ["Continental", "North Indian", "Biryani"],
+      averageCost: 200,
+    },
+    {
+      _id: "12346",
+      name: "FreshMenu",
+      isPro: 20,
+      isOff: 50,
+      durationOfDelivery: 51,
+      photos: [
+        "https://b.zmtcdn.com/data/pictures/chains/9/59179/a27b0a7128ad0035df8c14fabf52cdef_o2_featured_v2.jpg?output-format=webp",
+      ],
+      restaurantReviewValue: 3.8,
+      cuisine: ["Continental", "North Indian", "Biryani"],
+      averageCost: 200,
+    },
+  ]);
   return (
     <>
       <DeliveryCarousel />
+      {/* <Brand /> */}
+      <div className="flex flex-wrap justify-between">
+        {restList.map((restaurant) => (
+          <RestaurantCard {...restaurant} key={restaurant._id} />
+        ))}
+      </div>
     </>
   );
 };
